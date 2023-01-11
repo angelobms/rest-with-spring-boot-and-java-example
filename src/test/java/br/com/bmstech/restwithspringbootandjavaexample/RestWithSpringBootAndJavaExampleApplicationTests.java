6e53b90f28f4aaedc0e95cadddc1d8045edbb817
Angelo Santos <angelobms@gmail.com>
2023-01-11 19:11:51 -0300

    Initial project

---
+++ src/test/java/br/com/bmstech/restwithspringbootandjavaexample/RestWithSpringBootAndJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bmstech.restwithspringbootandjavaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
